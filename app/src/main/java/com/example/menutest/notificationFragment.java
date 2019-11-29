6cb279e16a7c9d21c1721ecef02ab383568e0a0f
Arkhanzu <32307403+Arkhanzu@users.noreply.github.com>
2019-11-29 13:18:49 -0800

    Notification Layout/Adapter (beginning)

--- app/src/main/java/com/example/menutest/notificationFragment.java
+++ app/src/main/java/com/example/menutest/notificationFragment.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class notificationFragment extends Fragment {
+
+    ListView listNotifications;
 
     public notificationFragment() {
     }
@@ -18,7 +22,7 @@ public class notificationFragment extends Fragment {
     public View onCreateView( LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.fragment_notification, container, false);
 
-
+       listNotifications = (ListView) view.findViewById(R.id.listNotifications);
 
         return view;
     }
